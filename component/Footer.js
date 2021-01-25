d4e60906879229712b74fc1a4e3583189095a8e4
v1pu2 <v1bhalodia@gmail.com>
2021-01-25 18:36:23 +0530

    clean textinput after adding value into the list

--- component/Footer.js
+++ component/Footer.js
@@ -1,62 +1,69 @@
-import React from 'react';
-import { View,Text, StyleSheet, TextInput, Button } from 'react-native';
-import Context from '../context/Context';
+import React from "react";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import Context from "../context/Context";
 
-export default class Footer extends React.Component{
-  constructor(props){
-   super(props);
-   this.state = {
-   newTask: ''
-   }
+export default class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newTask: "",
+    };
   }
+ 
   static contextType = Context;
-  render(){
-   return(
-    <View style={styles.view}>
-     <TextInput 
-      style={styles.input} 
-      onChangeText={(x)=>{this.setState({newTask: x})}}
-      placeholder="Add a new Task" 
-     />
-     <Text
-      style={styles.add} 
-      onPress={()=>this.context.addNewTask(this.state.newTask)}
-      >
-       Add
-      </Text>
-     </View>
+  render() {
+    return (
+      <View style={styles.view}>
+        <TextInput
+          style={styles.input}
+          onChangeText={(x) => {
+            this.setState({ newTask: x });
+          }}
+          placeholder='Add a new Task'
+          value={this.state.newTask}
+        />
+        <Text
+          style={styles.add}
+          onPress={() => {
+            this.context.addNewTask(this.state.newTask);
+            this.setState({ newTask: "" });
+          }}
+        >
+          Add
+        </Text>
+      </View>
     );
-   }
+  }
 }
 const styles = StyleSheet.create({
- container: {
-  flex: 1,
-  flexDirection:'column',
- },
- view: {
-  flex: 1,
-  width: '100%',
-  bottom: 20,
-  position: 'absolute',
-  flexDirection: 'row'
- },
- title: {
-  marginTop:20,
-  marginLeft: 20,
-  fontSize: 30,
- },
- input: {
-  width:'75%',
-  borderRadius: 30,
-  borderWidth:1,
-  padding: 5,
-  marginLeft: 10,
-  fontSize: 18
- },
- add: {
-  fontSize: 20,
-  padding: 5,
-  position: 'absolute',
-  right: '10%'
- }
+  container: {
+    flex: 1,
+    flexDirection: "column",
+  },
+  view: {
+    flex: 1,
+    width: "100%",
+    bottom: 20,
+    position: "absolute",
+    flexDirection: "row",
+  },
+  title: {
+    marginTop: 20,
+    marginLeft: 20,
+    fontSize: 30,
+  },
+  input: {
+    width: "75%",
+    borderRadius: 30,
+    borderWidth: 1,
+    padding: 5,
+    marginLeft: 10,
+    fontSize: 18,
+  },
+  add: {
+    fontSize: 20,
+    padding: 5,
+    position: "absolute",
+    right: "10%",
+  },
 });
